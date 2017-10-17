1b41f31c92b6af2a43fc485c6f30c4528fe0c922
Jess <jessbodie@gmail.com>
2017-10-16 22:44:54 -0400

    Foundation for error handling

--- js/index.js
+++ js/index.js
@@ -1,63 +1,108 @@
-// TODO  UI
-// 2. Move focus of cursor through form fields.
 
+// Set rate of inflation and rate of return based on decades-long averages
 const rateReturn = .073;
 const rateInflation = .033;
 
 function eventListeners() {
-     document.addEventListener('DOMContentLoaded', reset);
-     document.getElementById('calculate').addEventListener('click', captureCalculate);
+     document.addEventListener('DOMContentLoaded', testData);
+     document.getElementById('calculate').addEventListener('click', captureData);
+     document.getElementById('calculate').addEventListener('click', secondaryListeners);
+     document.getElementById('calculate').addEventListener('click', addErrorDiv);
+     // TODO Add Enter key to mimic Calculate button
+
 }
 
-function reset() {
-     document.getElementById('age').value = "";
-     document.getElementById('age-retire').value = "";
-     document.getElementById('age-death').value="";
-     document.getElementById('saved').value="$";
-     document.getElementById('expenses').value="$";
+// After initial calc, listen for changes to data and auto-update
+function secondaryListeners() {
+     var questionList = document.querySelectorAll('.entry-box');
+     for (var i = 0; i < questionList.length; i++) {
+          questionList[i].addEventListener('change', captureData);
+     }
 }
 
 
-function captureCalculate() {
+
+function captureData() {
      // Capture data user entered and error handling
      let age = document.getElementById('age').value;
-     parseFloat(age);
-     if (!age) {
+     if (!isValidAge(age)) {
           alert("Please enter your age.");
      }
      
      let ageRetire = document.getElementById('age-retire').value;
-     if (!ageRetire) {
+     if (!isValidAge(ageRetire)) {
           document.getElementById('age-retire').value = 65;
      }
-     parseFloat(ageRetire);
-     
+          
      let ageDeath = document.getElementById('age-death').value;
-     if (!ageDeath) {
+     if (!isValidAge(ageDeath)) {
           document.getElementById('age-death').value = 85;
      }
-     parseFloat(ageDeath);
-     
+          
      let saved = document.getElementById('saved').value;
-     if (!saved) {
+     // ASK IAN ABOUT THESE IF STATEMENTS
+     if (!isValidAmount(saved)) {
           document.getElementById('saved').value = '$0';
      }
-     if (saved.includes('$')) {
-          saved = saved.replace('$', '');
-     }
-     parseFloat(saved);
-     
+          
      let expenses = document.getElementById('expenses').value;
-     if (!expenses) {
+     if (!isValidAmount(expenses)) {
           alert("Please add your approximate annual expenses.");
      }
-     if (expenses.includes('$')) {
-          expenses = expenses.replace('$', '');
+     
+     var newDiv = document.createElement("div");
+     
+     switch (true) {
+          case (age > ageRetire):
+               var testoo = document.querySelector('.question-text').appendChild('<div class="error"><b>butt</b></div>');
+               newDiv.appendChild(document.createTextNode("So much testing"));
+               console.log("Uh oh, looks like there's something funny about your current and retirement ages. Please take a look.");
+          case (ageRetire > ageDeath):
+               // alert("Doah! Please check your retirement age or life expectancy.");
+               document.getElementById('age-retire').innerHTML = '</input><div class="error">ageretire</div>';
+
+          case (saved < 0):
+               alert("Hmmm, something's not right with how much you currently have saved. Please take a look.");
+
+          case (expenses < 0):
+               alert("Yikes... something's not rightwith your approximate expenses. Please check.");
+          default:
+               {
+               console.log(`Your age is ${age}. You will retire at ${ageRetire}. You will die at ${ageDeath}. You have ${saved} saved for retirement. You have ${expenses} in expenses.`);
+               calculate(age, ageRetire, ageDeath, saved, expenses);
+          }
      }
-     parseFloat(expenses);
+
+     // TODO More error checking?: return str.replace (/^\s+|\s+$/g, '');
+
+}
+
+
+function addErrorDiv () {
+     clearError();
      
-     console.log(`Your age is ${age}. You will retire at ${ageRetire}. You will die at ${ageDeath}. You have ${saved} saved for retirement. You have ${expenses} in expenses.`);
-     
+     // Create new div with error text
+     var errorDiv = document.createElement('div');
+     errorDiv.className = ('error');
+     var errorMessage = document.createTextNode('WORK WORK');
+     errorDiv.appendChild(errorMessage); //add the text node to the newly created div.
+
+     // Place new error div under corresponding field
+     var errorDivCurrent = document.getElementById('saved').parentNode.parentNode;
+     var errorDivNext = document.getElementById('saved').parentNode.nextElementSibling;
+     errorDivCurrent.insertBefore(errorDiv, errorDivNext);
+}
+
+// Clear previously shown errors
+function clearError () {
+     var errorList = document.querySelectorAll('.error');
+     for (var i = 0; i < errorList.length; i++) {
+          errorList[i].remove();
+     }
+}
+
+
+function calculate (age, ageRetire, ageDeath, saved, expenses) {
      // Calc time periods in preparation for the PV and FV calcs
      let yearsUntilRetirement = ageRetire - age;
      let yearsWithdrawing = ageDeath - ageRetire;
@@ -93,18 +138,59 @@ function captureCalculate() {
 
      // On click show amount needed to save
      document.querySelector('.results').innerHTML = (`You should be saving about <strong><font color="#21BFE1">${saveAnnual.toLocaleString('en-US', { style: 'currency', currency: 'USD', maximumSignificantDigits: 4 })}</font></strong> per year.`);
+     console.log(`And you should save ${saveAnnual} each year.`);
      
 }
 
+// Validate age inputs
+function isValidAge (a) {
+     if (!a) {
+          return false;
+     } else {
+          parseFloat(a);
+          return a;
+     }
+}
+
+// Validate dollar amounts inputs
+function isValidAmount (amt) {
+     if (!amt) {
+          return false;
+     } else if (amt.includes('$') || amt.includes(',')) {
+          amt = amt.replace('$', '');
+          amt = amt.replace(',', '');
+     }
+     parseFloat(amt);
+     return amt;
+}
+
+function reset() {
+     document.getElementById('age').value = "";
+     document.getElementById('age-retire').value = "";
+     document.getElementById('age-death').value="";
+     document.getElementById('saved').value="$";
+     document.getElementById('expenses').value="$";
+}
 
 
+function test () {
+     console.log("test");
+}
 
+function testData () {
+     document.getElementById('age').value = 30;
+     document.getElementById('age-retire').value = 65;
+     document.getElementById('age-death').value = 85;
+     document.getElementById('saved').value = 0;
+     document.getElementById('expenses').value = 10000;
+}
 
 
 eventListeners();
 
 
 
+///////////////////////////////////////////////////////////////
 // Key Formulas to port from 2013 JAVA and Excel versions:
 // Excel FV: interest_rate, number_payments, payment, PV, Type
 // double fv = FV(0.033, i - age, 0.0, annual_expenses, 0);
